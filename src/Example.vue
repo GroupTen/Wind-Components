c918de409cc92529cc8cad8d2dbd37bedbc87d36
Stephen <s.mark.toms@gmail.com>
2020-11-06 16:33:55 -0500

    updated example vue demo

--- src/Example.vue
+++ src/Example.vue
@@ -1,17 +1,16 @@
 <template>
-    <div>
-        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-        <Spinner/>
-    </div>
+  <div>
+    <link
+      href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+      rel="stylesheet"
+    />
+    <LLoadingSpinner />
+  </div>
 </template>
 <script>
-import Alert from './components/elements/loading/Spinner'
 export default {
-    components:{
-        Alert
-    },
-    mounted() {
-      this.$refs.alert.showAlert('success', 'test message')  
-    }
+  mounted() {
+    this.$refs.alert.showAlert('success', 'test message')
+  },
 }
 </script>
