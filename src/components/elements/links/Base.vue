4e4fe5a97110b776b42b7850fbb20c2c97d5be9c
Stephen <s.mark.toms@gmail.com>
2020-11-11 16:25:37 -0500

    updated base links to use config for classes

--- src/components/elements/links/Base.vue
+++ src/components/elements/links/Base.vue
@@ -33,12 +33,6 @@
   </div>
 </template>
 <script>
-const classes = {
-  primary:
-    'flex active:text-primary-700 font-medium text-primary-600 hover:text-primary-800 transition duration-150 ease-in-out h-full',
-  danger:
-    'flex active:text-danger-500 font-medium text-danger-500 hover:text-danger-600 transition duration-150 ease-in-out h-full',
-}
 export default {
   props: {
     type: {
@@ -69,7 +63,7 @@ export default {
   },
   computed: {
     classes() {
-      return classes[this.type]
+      return this.$wind.links.base[this.type]
     },
     svgComponent() {
       return this.icon && require(`@/static/icons/${this.icon}.svg?inline`)
