4df84c5f4965075f7c992d6caca40a01c4581ba2
manthann3 <manthansharma35@gmail.com>
2024-05-20 15:55:49 +0530

    WAC-4313: made changes for Horizontal

--- src/components/elements/steps/Horizontal.vue
+++ src/components/elements/steps/Horizontal.vue
@@ -5,7 +5,7 @@
 -->
   <nav>
     <ul
-      class="list-none border-2 border-primary-300 rounded-md divide-y divide-primary-300 md:flex justify-around md:divide-y-0"
+      class="list-none border-2 border-coolGray-100 rounded-md divide-y divide-red-500 md:flex justify-around md:divide-y-0"
     >
       <li
         v-for="(step, index) in steps"
@@ -15,7 +15,7 @@
       >
         <!-- Current Step -->
         <div
-          class="px-2 py-4 flex md:flex-col lg:flex-row items-center text-sm leading-5 font-medium space-x-4 md:space-x-0 lg:space-x-4 text-primary-600"
+          class="px-2 py-4 flex md:flex-col lg:flex-row items-center text-sm leading-5 font-medium space-x-4 md:space-x-0 lg:space-x-4 text-coolGray-300"
         >
           <div
             v-if="step.status === 'complete'"
@@ -24,7 +24,7 @@
                 ? 'w-10 h-10 md:w-12 md:h-12'
                 : 'w-6 h-6 md:w-8 md:h-8'
             "
-            class="flex-shrink-0 flex items-center justify-center bg-primary-600 rounded-full group-hover:bg-primary-800 transition ease-in-out duration-150"
+            class="flex-shrink-0 flex items-center justify-center bg-primary-500 rounded-full group-hover:bg-primary-800 transition ease-in-out duration-150"
           >
             <svg
               class="w-6 h-6 text-white"
@@ -44,8 +44,8 @@
             v-else
             :class="[
               step.status === 'current' || step.status === 'complete'
-                ? 'flex-shrink-0  flex justify-center border-primary-600 rounded-full'
-                : 'flex-shrink-0 flex justify-center border-2 rounded-full border-primary-600',
+                ? 'flex-shrink-0  flex justify-center border-primary-500 rounded-full'
+                : 'flex-shrink-0 flex justify-center border-2 rounded-full border-coolGray-100',
 
               pillSize === 'large'
                 ? 'w-10 h-10 md:w-12 md:h-12'
@@ -55,8 +55,8 @@
             <span
               :class="[
                 step.status === 'current'
-                  ? 'text-primary-900'
-                  : 'my-auto text-primary-600',
+                  ? 'text-primary-500'
+                  : 'my-auto text-coolGray-300',
               ]"
             >
               <span
@@ -66,7 +66,7 @@
                     ? 'w-10 h-10 md:w-12 md:h-12'
                     : 'w-6 h-6 md:w-8 md:h-8'
                 "
-                class="flex-shrink-0 flex items-center justify-center bg-primary-600 rounded-full group-hover:bg-primary-800 transition ease-in-out duration-150"
+                class="flex-shrink-0 flex items-center justify-center bg-primary-500 rounded-full group-hover:bg-primary-800 transition ease-in-out duration-150"
                 :style="step.status === 'current' ? 'paddingRight: 2px' : null"
               >
                 <svg
@@ -88,7 +88,7 @@
           </div>
           <p
             :class="{
-              'text-primary-900': step.status === 'current',
+              'text-primary-500': step.status === 'current',
               '!mb-5 !pr-0': pillSize === 'large',
               '!pr-0': pillSize !== 'large',
             }"
@@ -107,7 +107,7 @@
           ]"
         >
           <svg
-            class="h-full w-full text-primary-600"
+            class="h-full w-full text-coolGray-100"
             viewBox="0 0 22 80"
             fill="none"
             preserveAspectRatio="none"
